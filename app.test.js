a0544e53540145fee510af979402b8bc94f2bad4
bene-coder <onyiriaghabenedict@gmail.com>
2025-12-10 18:01:30 +0100

    new file upload

---
+++ app.test.js
@@ -0,0 +1,10 @@
+// app.test.js
+const { add, multiply } = require('./app');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('multiplies 2 * 3 to equal 6', () => {
+  expect(multiply(2, 3)).toBe(6);
+});
